cdadc01d290a6cb0b579515b4de2e0724b898cfd
Vivek Solanki <vivek.nitsan@gmail.com>
2023-08-07 12:49:06 +0530

    [TASK] composer dependency changed

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,11 @@
 <?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 defined('TYPO3_MODE') || defined('TYPO3') or die();
-$extKey = 'ns_cookieyes';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', '[NITSAN] Cookieyes');
+$extKey = '';
+ExtensionManagementUtility::addStaticFile(
+    'ns_cookieyes',
+    'Configuration/TypoScript',
+    '[NITSAN] Cookieyes'
+);
