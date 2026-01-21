b91f55f9ab399717fac400dafa69e338cdbc3c1a
Saloni Thakkar <saloni.nitsan@gmail.com>
2026-01-21 10:26:56 +0530

    [TASK]V14 compitibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF['ns_cookieyes'] = [
     'version' => '2.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.0.0-13.4.99',
+            'typo3' => '6.0.0-14.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
